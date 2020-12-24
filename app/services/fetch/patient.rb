1946498aa96ce99479d15e765f5dbcae2c0e751e
brunomrlima <bruno.mrlima@gmail.com>
2020-12-24 17:03:41 -0500

    added pagination to header with api next url

--- app/services/fetch/patient.rb
+++ app/services/fetch/patient.rb
@@ -1,7 +1,7 @@
 class Fetch::Patient
   API_URL = "http://hapi.fhir.org/baseR4/Patient?birthdate=gt1950-01-01&_count=80"
 
-  attr_accessor :patients, :next_url, :data
+  attr_accessor :patients, :current_url, :next_url
 
   def initialize(api_url = API_URL)
     api_request(api_url)
@@ -13,7 +13,8 @@ class Fetch::Patient
       response = Net::HTTP.get(URI(api_url))
       response = JSON.parse(response)
       @patients = response["entry"]
-      @next_url = response.data["link"][1]["url"]
+      @current_url = response["link"][0]["url"]
+      @next_url = response["link"][1]["url"]
     rescue
       []
     end
